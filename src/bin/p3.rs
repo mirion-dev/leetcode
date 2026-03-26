6b438350ded7045e983863a937122bd9dd4e17f7
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-03-26 16:54:01 +0800

    Add P3

---
+++ src/bin/p3.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut l = 0;
+        let mut last = [0; 128];
+        s.as_bytes()
+            .iter()
+            .enumerate()
+            .map(|(r, &ch)| {
+                l = l.max(last[ch as usize]);
+                last[ch as usize] = r + 1;
+                (r - l + 1) as i32
+            })
+            .max()
+            .unwrap_or(0)
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::length_of_longest_substring("abcabcbb".to_string()), 3);
+    assert_eq!(Solution::length_of_longest_substring("bbbbb".to_string()), 1);
+    assert_eq!(Solution::length_of_longest_substring("pwwkew".to_string()), 3);
+}
