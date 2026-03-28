75af01325b19b0803219ac95b706b7ab179ef641
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-03-29 00:51:04 +0800

    Simplify P8

--- src/bin/p8.rs
+++ src/bin/p8.rs
@@ -1,19 +1,10 @@
 impl Solution {
     pub fn my_atoi(s: String) -> i32 {
         let mut s = s.trim_start().chars().peekable();
-        let sign = match s.peek() {
-            Some('+') => {
-                s.next();
-                1
-            }
-            Some('-') => {
-                s.next();
-                -1
-            }
-            None => return 0,
+        let sign = match s.next_if(|&ch| ch == '+' || ch == '-') {
+            Some('-') => -1,
             _ => 1,
         };
-
         s.map_while(|ch| ch.to_digit(10))
             .try_fold(0i32, |v, d| v.checked_mul(10).and_then(|v| v.checked_add(d as i32 * sign)))
             .unwrap_or(if sign == 1 { i32::MAX } else { i32::MIN })
@@ -29,6 +20,7 @@ fn main() {
     assert_eq!(Solution::my_atoi("0-1".to_string()), 0);
     assert_eq!(Solution::my_atoi("words and 987".to_string()), 0);
 
+    assert_eq!(Solution::my_atoi("".to_string()), 0);
     assert_eq!(Solution::my_atoi(i32::MAX.to_string()), i32::MAX);
     assert_eq!(Solution::my_atoi(i32::MIN.to_string()), i32::MIN);
     assert_eq!(Solution::my_atoi(i64::MAX.to_string()), i32::MAX);
