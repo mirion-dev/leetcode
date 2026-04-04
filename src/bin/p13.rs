2a07c71bc5109cee5d628240218960e38d548d1b
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-04-04 11:09:42 +0800

    Add P13

---
+++ src/bin/p13.rs
@@ -0,0 +1,32 @@
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        s.chars()
+            .rev()
+            .fold((0, 0), |(res, last), ch| {
+                let val = match ch {
+                    'I' => 1,
+                    'V' => 5,
+                    'X' => 10,
+                    'L' => 50,
+                    'C' => 100,
+                    'D' => 500,
+                    'M' => 1000,
+                    _ => 0,
+                };
+                if val >= last {
+                    (res + val, val)
+                } else {
+                    (res - val, val)
+                }
+            })
+            .0
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::roman_to_int("III".to_string()), 3);
+    assert_eq!(Solution::roman_to_int("LVIII".to_string()), 58);
+    assert_eq!(Solution::roman_to_int("MCMXCIV".to_string()), 1994);
+}
