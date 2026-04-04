fed26d601ac4ca06de74389e0ee8268fa5f6a9bd
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-04-04 09:53:24 +0800

    Add P122

---
+++ src/bin/p122.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        prices.windows(2).map(|w| (w[1] - w[0]).max(0)).sum()
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::max_profit(vec![7, 1, 5, 3, 6, 4]), 7);
+    assert_eq!(Solution::max_profit(vec![1, 2, 3, 4, 5]), 4);
+    assert_eq!(Solution::max_profit(vec![7, 6, 4, 3, 1]), 0);
+}
