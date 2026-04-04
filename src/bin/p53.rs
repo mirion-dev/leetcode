b9efb4a18bec2b6df679a33ea7530162dbaef5d4
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-04-04 10:06:23 +0800

    Add P53

---
+++ src/bin/p53.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        let mut last = 0;
+        nums.iter()
+            .map(|&i| {
+                last = (last + i).max(i);
+                last
+            })
+            .max()
+            .unwrap_or(0)
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
+    assert_eq!(Solution::max_sub_array(vec![1]), 1);
+    assert_eq!(Solution::max_sub_array(vec![5, 4, -1, 7, 8]), 23);
+}
