9cee0a60d22d7933d7e905c17aa75a1f9ebc9c72
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-04-04 11:26:02 +0800

    Add P58

---
+++ src/bin/p58.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        s.split_whitespace().last().map_or(0, |w| w.chars().count()) as i32
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::length_of_last_word("Hello World".to_string()), 5);
+    assert_eq!(Solution::length_of_last_word("   fly me   to   the moon  ".to_string()), 4);
+    assert_eq!(Solution::length_of_last_word("luffy is still joyboy".to_string()), 6);
+
+    assert_eq!(Solution::length_of_last_word("".to_string()), 0);
+    assert_eq!(Solution::length_of_last_word("word".to_string()), 4);
+}
