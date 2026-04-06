af08024f3c4fe36cdc6bf6ad1e09801b5a006cb4
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-04-06 22:33:42 +0800

    Add P274

---
+++ src/bin/p274.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn h_index(citations: Vec<i32>) -> i32 {
+        let n = citations.len();
+        let mut count = vec![0; n + 1];
+        for c in citations {
+            count[(c as usize).min(n)] += 1;
+        }
+
+        let mut sum = 0;
+        for (i, &val) in count.iter().enumerate().rev() {
+            sum += val;
+            if sum >= i {
+                return i as i32;
+            }
+        }
+        0
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::h_index(vec![3, 0, 6, 1, 5]), 3);
+    assert_eq!(Solution::h_index(vec![1, 3, 1]), 1);
+}
